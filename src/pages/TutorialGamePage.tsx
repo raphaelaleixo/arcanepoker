6ab2ba71de79ef394062215d49d42e7f07373fd4
Raphael Aleixo <raphael.aleixo@gmail.com>
2026-03-20 22:57:30 -0400

    fix: GameProvider must wrap TutorialProvider so useGame() has context

--- src/pages/TutorialGamePage.tsx
+++ src/pages/TutorialGamePage.tsx
@@ -1,11 +1,14 @@
 // src/pages/TutorialGamePage.tsx
+import { GameProvider } from "../store/GameContext";
 import { TutorialProvider } from "../tutorial/TutorialContext";
-import { GamePage } from "./GamePage";
+import { PokerTable } from "../components/Table/PokerTable";
 
 export function TutorialGamePage() {
   return (
-    <TutorialProvider>
-      <GamePage isTutorial />
-    </TutorialProvider>
+    <GameProvider isTutorial>
+      <TutorialProvider>
+        <PokerTable />
+      </TutorialProvider>
+    </GameProvider>
   );
 }
