9a3cf7d857b634323ce3d3ec47e5198b152aea98
Raphael Aleixo <raphael.aleixo@gmail.com>
2026-03-26 21:13:57 -0400

    feat: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material";
+import { Analytics } from "@vercel/analytics/react";
 import { theme } from "./theme";
 import "./index.css";
 import App from "./App.tsx";
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <App />
+        <Analytics />
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
