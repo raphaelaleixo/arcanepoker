4ced536687df94f4b2d6460e2fd3a1e37c4c5c7c
Raphael Aleixo <raphael.aleixo@gmail.com>
2026-04-10 16:09:28 -0400

    feat: wire AudioPreferencesProvider and useGameSounds into app

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material";
 import { Analytics } from "@vercel/analytics/react";
 import { theme } from "./theme";
+import { AudioPreferencesProvider } from "./store/AudioPreferencesContext";
 import "./index.css";
 import App from "./App.tsx";
 
@@ -11,7 +12,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
-        <App />
+        <AudioPreferencesProvider>
+          <App />
+        </AudioPreferencesProvider>
         <Analytics />
       </ThemeProvider>
     </BrowserRouter>
