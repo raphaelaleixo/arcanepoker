255c7b757ff6c5638fe371ea100036c54ccd3a56
Raphael Aleixo <raphael.aleixo@gmail.com>
2026-04-10 23:24:22 -0400

    feat: add /settings route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { GamePage } from "./pages/GamePage";
 import { HomePage } from "./pages/HomePage";
 import { RulesPage } from "./pages/RulesPage";
 import { TutorialGamePage } from "./pages/TutorialGamePage";
+import { SettingsPage } from "./pages/SettingsPage";
 // import { NavFab } from "./components/NavFab";
 
 const demoEnabled = import.meta.env.VITE_ENABLE_DEMO === "true";
@@ -35,6 +36,7 @@ export default function App() {
         <Route path="/game" element={<GamePage />} />
         <Route path="/tutorial" element={<TutorialGamePage />} />
         <Route path="/rules" element={<RulesPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
         {demoEnabled && DemoPage && (
           <Route path="/demo" element={<DemoPage />} />
         )}
